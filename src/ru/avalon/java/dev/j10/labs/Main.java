7b8f3b47dd506291ce2becb398e9bf3bbb85f2ad
Lenovo Z5070 <Lenovo Z5070@Lenovo-PC>
2020-11-13 18:33:33 +0300

    class Main

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -1,5 +1,7 @@
 package ru.avalon.java.dev.j10.labs;
 
+import ru.avalon.java.dev.j10.labs.commons.Address;
+import ru.avalon.java.dev.j10.labs.models.Passport;
 import ru.avalon.java.dev.j10.labs.models.Person;
 
 public class Main {
@@ -7,22 +9,26 @@ public class Main {
     /*
      * FIXME(Студент): Измените определение метода так, чтобы он стал точкой входа в приложение.
      */
-    Main() {
-
-        Person ivanov = null;
-        Person smith = null;
-
+    public static void main(String[] args) {
+        
         /*
          * TODO(Студент): Создайте экземпляры класса 'Person'
          *
          * 1. Проинициализируейте переменную 'ivanov', так,
          *    чтобы она адресовала объект типа 'Person'
-         *    описывающий человека по имени 'Иванов Иван Иванович'.
-         *
+         *    описывающий человека по имени 'Иванов Иван Иванович'.*/
+        
+        Person ivanov = new Person(new Passport("Иванов ", "Иван ", "Иванович"), 
+                new Address ("resAddressIvanov", "regAddressIvanov"));
+              
+        /*
          * 2. Проинициализируейте переменную 'smith', так,
          *    чтобы она адресовала объект типа 'Person'
          *    описывающий человека по имени 'John Edvard Smith'.
          */
+        
+        Person smith = new Person(new Passport("John", "Edward", null, "Smith"), 
+                new Address("resAddressSmith", "regAddressSmith"));
 
         /*
          * TODO(Студент): Создайте несколько строковых переменных:
@@ -30,19 +36,30 @@ public class Main {
          * 1. Строковую переменную, содержащую полное имя
          *    человека, описанного объектом, адресованным
          *    переменной 'ivanov'.
-         *
-         * 2. Строковую переменную, содержащую полное имя
-         *    человека, описанного объектом, адресованным
-         *    переменной 'smith'.
-         *
-         * 3. Строковую переменную, содержащую адрес проживания
-         *    человека, описанного объектом, адресованным
-         *    переменной 'ivanov'.
-         *
-         * 4. Строковую переменную, содержащую адрес проживания
+         */
+        
+        String nameIvanov = ivanov.getFullName();
+        
+         /* 2. Строковую переменную, содержащую полное имя
          *    человека, описанного объектом, адресованным
          *    переменной 'smith'.
          */
+         
+         String nameSmith = smith.getFullName();
+         
+         /* 3. Строковую переменную, содержащую адрес проживания
+         *    человека, описанного объектом, адресованным
+         *    переменной 'ivanov'.
+         */
+         
+         String addressIvanov = ivanov.getAddress();
+         
+         /* 4. Строковую переменную, содержащую адрес проживания
+         *    человека, описанного объектом, адресованным
+         *    переменной 'smith'.
+         */
+         
+         String addressSmith = smith.getAddress();
 
         /*
          * TODO(Студент): Выведите в консоль значения созданных строковых переменных:
@@ -50,5 +67,10 @@ public class Main {
          * Значение каждой переменной должно быть выведено на
          * отдельной строке.
          */
+        
+        System.out.println(nameIvanov + "\n");
+        System.out.println(nameSmith + "\n");
+        System.out.println(addressIvanov + "\n");
+        System.out.println(addressSmith + "\n");
     }
 }
